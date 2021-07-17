df5eb790568240a3a1622b08fd5fec5a2d0547a0
mariasr1308 <52265827+mariasr1308@users.noreply.github.com>
2021-07-17 14:17:55 +0200

    test random seed

--- examples/question-answering/run_qa.py
+++ examples/question-answering/run_qa.py
@@ -669,8 +669,8 @@ def main():
         return model
     
     trainer = QuestionAnsweringTrainer(
-#         model_init=model_init,
-        model=model,
+        model_init=model_init,
+#         model=model,
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
